43584f748a59dbc298dfd3f860d38c555a3c0b57
Diego Alejandro Sánchez Bernal <diegosan861@gmail.com>
2023-05-24 23:37:19 -0500

    Adición de la funcionalidad para desencriptar un texto.

--- index.js
+++ index.js
@@ -24,3 +24,29 @@ function encriptar (){
         alert ("Debes ingresar algún texto");
     }
 }
+
+function desencriptar (){
+    let texto = document.getElementById("texto").value;
+    let tituloMensaje = document.getElementById("titulo-mensaje");
+    let parrafo = document.getElementById("parrafo");
+    let muñeco = document.getElementById("muñeco");
+
+    let textoCifrado = texto
+    .replace(/ara/gi, "a")
+    .replace(/ete/gi, "e")
+    .replace(/imi/gi, "i")
+    .replace(/ovo/gi, "o")
+    .replace(/ufu/gi, "u");
+
+    if (texto.length !=0){
+        document.getElementById("texto").value = textoCifrado;
+        tituloMensaje.textContent = "Texto desencriptado con éxito";
+        parrafo.textContent = "";
+        muñeco.src = "./img/desencriptado.jpg";       
+    }else {
+        muñeco.src = "./img/muñeco.png";
+        tituloMensaje.textContent = "Ningún mensaje fue encontrado";
+        parrafo.textContent = "Ingresa el texto que deseas encriptar o desencriptar"
+        alert ("Debes ingresar algún texto");
+    }
+}
